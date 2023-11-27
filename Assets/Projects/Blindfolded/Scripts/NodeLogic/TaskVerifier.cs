47a1d5619041a022fe055e3daaaa7d788cc7d0bb
DHindriks <dl.hindriks@gmail.com>
2023-11-27 16:19:38 +0100

    Node event optional completion requirement

--- Assets/Projects/Blindfolded/Scripts/NodeLogic/TaskVerifier.cs
+++ Assets/Projects/Blindfolded/Scripts/NodeLogic/TaskVerifier.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TaskVerifier : MonoBehaviour
 {
     public string Task_Number;
     public NodeManager nodeManager;
+    [SerializeField] bool RequireNodeContact = true;
+    [SerializeField] bool RequireEventComplete = false;
+
+    [HideInInspector]public bool NodeContacted = false;
+    [HideInInspector]public bool EventCompleted = false;
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && nodeManager.StoryProgressionNumber != 7)
+        if (other.CompareTag("Player"))
         {
-            if (nodeManager.CurrentObjectiveNodeString == Task_Number)
+            NodeContacted = true;
+            if (!RequireEventComplete || RequireEventComplete && EventCompleted)
             {
-                Debug.Log("Hit");
-                nodeManager.StoryHasReachedWaypoint = (true);
-                nodeManager.StoryProgressionBookmark();
+                CompleteTask();
             }
         }
     }
+
+    public void EventComplete()
+    {
+        EventCompleted = true;
+        if (!RequireNodeContact || RequireNodeContact && NodeContacted)
+        {
+            CompleteTask();
+        }
+    }
+
+    void CompleteTask()
+    {
+        if (nodeManager.StoryProgressionNumber != 7 && nodeManager.CurrentObjectiveNodeString == Task_Number)
+        {
+            Debug.Log("Hit");
+            nodeManager.StoryHasReachedWaypoint = (true);
+            nodeManager.StoryProgressionBookmark();
+        }
+    }
+
 }
