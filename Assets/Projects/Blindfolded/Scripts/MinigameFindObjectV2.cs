dd236944cc2a4689c7840e897c55e78537ca9f9f
DHindriks <dl.hindriks@gmail.com>
2024-01-19 16:43:24 +0100

    Update v0.0.39

--- Assets/Projects/Blindfolded/Scripts/MinigameFindObjectV2.cs
+++ Assets/Projects/Blindfolded/Scripts/MinigameFindObjectV2.cs
@@ -7,17 +7,41 @@ public class MinigameFindObjectV2 : MinigameBase
 
     [SerializeField] GameObject ObjectToFind;
 
+    [SerializeField] List<GameObject> Interiors;
+
     [SerializeField] List<GameObject> SpawnPoints;
 
     [SerializeField] AudioSource SoundObj;
     [SerializeField] AudioSource VictorySoundObj;
 
+    [SerializeField] int ScoreReqForComplete = 0;
+
     bool Init = false;
+
+    int CurrentScore;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (SpawnPoints.Count <= 0 && Interiors.Count > 0)
+        {
+            GameObject interior = Instantiate(Interiors[Random.Range(0, Interiors.Count)]);
+            interior.transform.SetParent(transform);
+            interior.transform.position = transform.position;
+            foreach (Transform spawnpoint in interior.transform.GetChild(0))
+            {
+                SpawnPoints.Add(spawnpoint.gameObject);
+            }
+        }else
+        {
+            Debug.LogWarning("No spawnpoints or interiors set.");
+        }
         ResetMinigame();
+    }
+
+    public void CompleteMinigame()
+    {
+        gameObject.SetActive(false);
     }
 
     public void ResetMinigame()
@@ -26,6 +50,13 @@ public class MinigameFindObjectV2 : MinigameBase
         if (Init)
         {
             VictorySoundObj.Play();
+            CurrentScore++;
+
+            if (CurrentScore >= ScoreReqForComplete)
+            {
+                OnMinigameComplete.Invoke();
+            }
+
         }else
         {
             Init = true;
