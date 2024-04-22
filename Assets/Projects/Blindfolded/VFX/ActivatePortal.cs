0c6797f98591930ac8b693e8b02c1af7ebb912e4
ChrisBorn <96482354+kokiad@users.noreply.github.com>
2024-04-22 15:01:16 +0200

    vfx in
    i made the vfx play on player trigger enter but we nee dto add it to the node and make the node colldier the size of the portal thingy and we have to wait a certain amount of second forthe vfx to play before teleporting to the second scene als oteleporting in the second scene have one proble that the player is spawned below the map

--- Assets/Projects/Blindfolded/VFX/ActivatePortal.cs
+++ Assets/Projects/Blindfolded/VFX/ActivatePortal.cs
@@ -3,13 +3,20 @@ using UnityEngine.VFX;
 
 public class ActivatePortal : MonoBehaviour
 {
-    public VisualEffect vfx; // Reference to your Visual Effect component
+    public VisualEffect vfx; // Reference to your Visual Effect Graph component
+    private Collider triggerCollider; // Reference to the Collider component
+
+    private void Start()
+    {
+    
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // Assuming your player has a "Player" tag
         {
-            vfx.Play(); // Play the Visual Effect
+            // Play the Visual Effect
+            vfx.Play();
         }
     }
 }
