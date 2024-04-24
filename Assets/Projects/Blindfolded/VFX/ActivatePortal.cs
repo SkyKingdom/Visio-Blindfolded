63fa8c951f2dedbf4f1c9db8ac730be5142b3d28
pjboer1 <boer.pim77@gmail.com>
2024-04-24 13:49:44 +0200

    - UI is now working , Thank the heavens. - Added a timer for the VFX so the animations plays first and then you get teleported (we may want to freeze the player here?)

--- Assets/Projects/Blindfolded/VFX/ActivatePortal.cs
+++ Assets/Projects/Blindfolded/VFX/ActivatePortal.cs
@@ -5,10 +5,12 @@ public class ActivatePortal : MonoBehaviour
 {
     public VisualEffect vfx; // Reference to your Visual Effect Graph component
     private Collider triggerCollider; // Reference to the Collider component
-
+    private Timer animationTimer;
+    [SerializeField] private Levels.levels level;
+    
     private void Start()
     {
-    
+      animationTimer = new Timer();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,6 +19,17 @@ public class ActivatePortal : MonoBehaviour
         {
             // Play the Visual Effect
             vfx.Play();
+            animationTimer.SetTimer(3);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (animationTimer.isActive && animationTimer.TimerDone())
+        {
+            animationTimer.StopTimer();
+            GameManager.SceneLoader(level);
         }
     }
 }
