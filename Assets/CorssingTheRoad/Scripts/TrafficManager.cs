40bd1a9ec842e1c21cf43795942dceb85a926333
kokiad <96482354+kokiad@users.noreply.github.com>
2024-05-14 14:28:23 +0200

    Multiple Lanes Adding Option
    I updated the Traffic Manager Script, so that you can add spawn points (in other words: lanes) and I made the parameters a class which you can also add to each spawn point, so each lane has different parameters of spawning the cars. If you add a parameter to the list but do not assign values it will get the default values from the scripts, and if you have more parameters than spawn points that is not a problem.

--- Assets/CorssingTheRoad/Scripts/TrafficManager.cs
+++ Assets/CorssingTheRoad/Scripts/TrafficManager.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrafficManager : MonoBehaviour
+[System.Serializable]
+public class SpawnPointParameters
 {
-    public GameObject[] carPrefabs; //Cars List
-    public AudioClip[] carSounds; //Car Sounds List
-    public Transform spawnPoint;
     public float minSpawnDelay = 0.5f;
     public float maxSpawnDelay = 5f;
-    public float initialSpawnDelay = 1f; //If we want to have an increase
+    public float initialSpawnDelay = 1f;        //If we want to have an increase
     public float spawnDelayIncreaseRate = 0.1f; //If we want to make it easier for the player we can increase the spawn delay overtime
                                                 //This way the traffic gets less with time passing and it is not fully random
+    public float spawnDistance = 1f;            // Distance at which the traffic manager checks for space to spawn a new car
 
-    public float spawnDistance = 5f; // Distance at which the traffic manager checks for space to spawn a new car
+}
+
+public class TrafficManager : MonoBehaviour
+{
+    public GameObject[] carPrefabs;  //Cars List
+    public AudioClip[] carSounds;    //Car Sounds List
+    public Transform[] spawnPoints;  //Option to add more lanes
+    public SpawnPointParameters[] spawnPointParameters; // Parameters for each spawn point
+
 
     void Start()
     {
@@ -24,29 +31,29 @@ public class TrafficManager : MonoBehaviour
     {
         while (true)
         {
-            if (ScreensActivation.instance != null)
+            if (ScreensActivation.instance != null && ScreensActivation.instance.isActive)
             {
-                if (ScreensActivation.instance.isActive)
+                for (int i = 0; i < spawnPoints.Length; i++)
                 {
-                    float spawnDelay = GetSpawnDelay();
+                    float spawnDelay = GetSpawnDelay(i); //Pass index to get parameters for the specific spawn point
                     yield return new WaitForSeconds(spawnDelay);
 
                     // Check if there's enough space to spawn a car
-                    if (CheckSpaceForCar())
+                    if (CheckSpaceForCar(i)) //Pass index to check space for the specific spawn point
                     {
                         // Spawn a car
-                        SpawnCar();
+                        SpawnCar(i);  //Pass index to spawn car at the specific spawn point
                     }
                 }
-                else
-                {
-                    break;
-                }
+            }
+            else
+            {
+                break;
             }
         }
     }
 
-    private float GetSpawnDelay()
+    private float GetSpawnDelay(int index)
     {
         // Example: Increase spawn delay gradually over time
         float time = Time.timeSinceLevelLoad;
@@ -58,45 +65,44 @@ public class TrafficManager : MonoBehaviour
         if (time < initialSpawnDuration)
         {
             // Gradually decrease the spawn delay during the initial spawn duration
-            float decreasedDelay = Mathf.Clamp(initialSpawnDelay + time * spawnDelayIncreaseRate, minSpawnDelay, maxSpawnDelay);
+            float decreasedDelay = Mathf.Clamp(spawnPointParameters[index].initialSpawnDelay + time * spawnPointParameters[index].spawnDelayIncreaseRate, spawnPointParameters[index].minSpawnDelay, spawnPointParameters[index].maxSpawnDelay);
             return decreasedDelay;
         }
         else
         {
             // Return a random spawn delay within the specified range
-            return Random.Range(minSpawnDelay, maxSpawnDelay);
+            return Random.Range(spawnPointParameters[index].minSpawnDelay, spawnPointParameters[index].maxSpawnDelay);
         }
         //return Mathf.Clamp(initialSpawnDelay + time * spawnDelayIncreaseRate, minSpawnDelay, maxSpawnDelay); //If we want to have an increase
         //return Mathf.Clamp(initialSpawnDelay + time, minSpawnDelay, maxSpawnDelay); //No increase but idk
         //return Random.Range(minSpawnDelay, maxSpawnDelay); //I think that's the best way to do it
     }
 
-    private bool CheckSpaceForCar()
+    private bool CheckSpaceForCar(int index)
     {
         // Cast a ray forward to check for space
         RaycastHit hit;
-        if (!Physics.Raycast(spawnPoint.position, spawnPoint.
-            forward, out hit, spawnDistance))
+        if (!Physics.Raycast(spawnPoints[index].position, spawnPoints[index].forward, out hit, spawnPointParameters[index].spawnDistance))
         {
             // There's enough space to spawn a car
-            Debug.DrawRay(spawnPoint.position, spawnPoint.forward * spawnDistance, Color.green);
+            Debug.DrawRay(spawnPoints[index].position, spawnPoints[index].forward * spawnPointParameters[index].spawnDistance, Color.green);
             return true;
         }
         else
         {
             // There's not enough space to spawn a car
-            Debug.DrawRay(spawnPoint.position, spawnPoint.forward * hit.distance, Color.black);
+            Debug.DrawRay(spawnPoints[index].position, spawnPoints[index].forward * hit.distance, Color.black);
             return false;
         }
     }
 
-    private void SpawnCar()
+    private void SpawnCar(int index)
     {
         // Randomly select a car prefab
         GameObject randomCarPrefab = carPrefabs[Random.Range(0, carPrefabs.Length)];
 
         // Instantiate the selected car prefab
-        GameObject newCar = Instantiate(randomCarPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject newCar = Instantiate(randomCarPrefab, spawnPoints[index].position, spawnPoints[index].rotation);
 
         // Get the CarController component from the spawned car
         CarController carController = newCar.GetComponent<CarController>();
